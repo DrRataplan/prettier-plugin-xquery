eb2315d54297647c6097a18b53241c2436da8822
Martin Middel <martin@elliat.nl>
2025-05-23 10:05:32 +0200

    Set up a firebase hosting app
    We have a demo page! Now at https://prettier-plugin-xquery-demo.web.app/, later at a different
    domain as well

---
+++ demo/main.ts
@@ -0,0 +1,19 @@
+import prettier from 'prettier';
+import prettierPluginXQuery from '../src/main.ts';
+
+// Register the jinn-codemirror web component
+import '@jinntec/jinn-codemirror';
+
+const codemirror = document.getElementById('input')! as (HTMLElement & { content: string });
+
+const formatButton = document.getElementById('format');
+
+formatButton?.addEventListener('click', async () => {
+	const code = codemirror.content;
+	const result = await prettier.format(code, {
+		parser: 'xquery',
+		plugins: [prettierPluginXQuery],
+	});
+
+	codemirror.content = result;
+});
